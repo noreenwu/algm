8214e1494fb0134ef2acf5c9142ec1bdc2841d0d
Noreen Wu <noreenwu@gmail.com>
2020-04-26 15:04:26 -0400

    doc: added explanation and tweaked debug code for later review

---
+++ p1/explanation_1.md
@@ -0,0 +1,19 @@
+## Find Closest Square Root of Provided Number
+
+The first solution to come to mind may be to try each integer, from 1 on up,
+until the integer multiplied times itself yields the target number or goes over.
+If it goes over, then the previous number, the first one to not go over,
+would be the answer.
+
+As numbers get larger, the fraction of the numbers that have to be tested compared
+to the size of the number becomes smaller and smaller. So not terrible, less than O(N) 
+time complexity, but there is a better way, which is to do a binary search for the answer.
+
+## Binary Search Solution
+
+Take half of the provided number as the starting point and check if its square
+is higher or lower than the target. If higher, then search the smaller of the
+two spaces. Repeat halving the range of numbers to search, always checking
+if the midpoint of that range is the answer. This way of continuously diminishing
+the search space by half means that we will get the solution in O(log N) tries.
+
--- p1/problem_1.py
+++ p1/problem_1.py
@@ -22,22 +22,23 @@ def sqrt(number):
             return mid
 
         if mid * mid  < number:
-            print("we are under: look in upper half")
+            print("\twe are under")
             lower = mid+1
         if mid * mid > number: 
-            print("we are over: look in lower half: reset upper from {} to {}".format(upper, mid-1))
+            print("\twe are over: look in lower half")
             upper = mid-1
 
         mid = (upper + lower) // 2
-        print("new mid is {}".format(mid))
+        print("\tnew mid is {} in range {} to {}".format(mid, lower, upper))
         i += 1
 
 
     print("lower {} mid {} upper {}".format(lower, mid, upper))
     return mid
-    
+
 print ("Pass" if  (6 == sqrt(36)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
-# print ("Pass" if  (4 == sqrt(16)) else "Fail")
+print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
+print ("Pass" if  (25 == sqrt(625)) else "Fail")
