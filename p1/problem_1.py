9a4a826bf4a32db0477e30a93aeaaa00b55280a0
Noreen Wu <noreenwu@gmail.com>
2020-05-09 19:52:47 -0400

    fix/test: testing higher numbers and negative numbers; removed limit on number of interations

--- p1/problem_1.py
+++ p1/problem_1.py
@@ -2,6 +2,7 @@ def display_answer(num):
     """
     Uniformly display the provided answer and return it
     """
+
     print("\tAnswer: {}".format(num))
     return num
 
@@ -16,15 +17,20 @@ def sqrt(number):
     """
     print("Find sqrt of {}:".format(number))
 
+    if number < 0:
+        print("the square root of a negative number is not a real number")
+        return -1
+
     if number == 0 or number == 1:
         return display_answer(number)
         
+
     lower = 1
     mid = number // 2
     upper = number
 
     i = 0
-    while lower <= upper and i < 10:
+    while lower <= upper:
         if mid * mid == number:
             return display_answer(mid)
 
@@ -36,6 +42,7 @@ def sqrt(number):
         mid = (upper + lower) // 2
         i += 1
 
+
     return display_answer(mid)
 
 print ("Pass\n" if  (6 == sqrt(36)) else "Fail")
@@ -44,3 +51,8 @@ print ("Pass\n" if  (4 == sqrt(16)) else "Fail")
 print ("Pass\n" if  (1 == sqrt(1)) else "Fail")
 print ("Pass\n" if  (5 == sqrt(27)) else "Fail")
 print ("Pass\n" if  (25 == sqrt(625)) else "Fail")
+print ("Pass\n" if  (-1 == sqrt(-64)) else "Fail")              # not a real number message
+print ("Pass\n" if  (50 == sqrt(2500)) else "Fail")
+print ("Pass\n" if  (112 == sqrt(12544)) else "Fail")
+print ("Pass\n" if  (1122 == sqrt(1258884)) else "Fail")
+print ("Pass\n" if  (99999 == sqrt(9999800001)) else "Fail")
