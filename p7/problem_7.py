35e7585cf78003c742a8b360e45da4a0a37712d0
Noreen Wu <noreenwu@gmail.com>
2020-05-09 16:07:27 -0400

    reorg: moved router code into problem_7.py and documented test cases

--- p7/problem_7.py
+++ p7/problem_7.py
@@ -44,3 +44,69 @@ class RouteTrieNode:
         self.handler = handler
 
 
+# The Router class will wrap the Trie and handle 
+class Router:
+    def __init__(self, homepage='homepage handler'):
+        # Create a new RouteTrie for holding our routes
+        # You could also add a handler for 404 page not found responses as well!
+        self.routes = RouteTrie()
+        self.add_handler('/', homepage)
+
+    def add_handler(self, path, handler):
+        # Add a handler for a path
+        # You will need to split the path and pass the pass parts
+        # as a list to the RouteTrie
+        path_segs = self.split_path(path)
+        # print("adding {}".format(path_segs))
+        self.routes.insert(path_segs, handler)
+
+    def lookup(self, path):
+        # lookup path (by parts) and return the associated handler
+        # you can return None if it's not found or
+        # return the "not found" handler if you added one
+        # bonus points if a path works with and without a trailing slash
+        # e.g. /about and /about/ both return the /about handler
+        path_segs = self.split_path(path)
+        # print("looking up {}".format(path_segs))
+        if self.routes.find(path_segs) is None:
+            return "not found handler"
+
+        return self.routes.find(path_segs)
+
+    def split_path(self, path):
+        # you need to split the path into parts for 
+        # both the add_handler and loopup functions,
+        # so it should be placed in a function here 
+        if len(path) > 0:
+            if path[-1:] == '/':
+                path = path[:-1]
+            if len(path) > 0 and path[0] == '/':
+                path = path[1:]
+
+        path_segs = path.split('/')
+        if len(path_segs) == 0:
+            path_segs = [ '/']
+
+        return path_segs
+
+
+new_router = Router()
+
+new_router.add_handler("/home/about", "about handler")
+
+print("\tHandler: {}\n".format(new_router.lookup("/home/about"))) # about handler
+
+print("\tHandler: {}\n".format(new_router.lookup("/home/about/"))) # about handler (ignores trailing /)
+
+new_router.add_handler("/home/about/me", "about me handler")
+
+print("\tHandler: {}\n".format(new_router.lookup("/home/about/me"))) # about me handler
+
+new_router.add_handler("/12/14/19/news", "news handler")
+
+print("\tHandler: {}\n".format(new_router.lookup("/12/14/19/news"))) # news handler
+
+print("\tHandler: {}\n".format(new_router.lookup("")))  # homepagehandler
+
+print("\tthis route is not defined and lookup returns: {}\n".format(new_router.lookup("/something/else")))
+    # not found handler
